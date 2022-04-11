3132113cb541bc4709c1e6e692dafed240d47458
Hays4Days <gerushays@gmail.com>
2022-04-10 21:07:54 -0700

    questions fixed and readme started

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# timed-quiz
+# Javascript timed Quiz
+## Purpose
+The purpose of this webpage is to generate a random password between 8 and 128 characters. </br>
+Users have the option to select the length of their password and whether or not to include: Uppercase or Lowercase letters, numbers and special characters. </br>
+If the user inputs a number less than 8 or greater than 128 it will do nothing and they will have to click the generate button once more to select a number between the given length. </br>
+Once the user selects a length they will have 4 windows populate to confirm what characters they would like to include in their password.</br>
+If they confirm all 4 windows their password will include all 4 types of characters. </br>
+However, if they confirm 2 and cancel 2 the password will only include what characters they have confirmed that will populate in the text box. </br>
+
+## Built With
+* HTML
+* CSS
+* JS
+
+## Moving Forward
+I have included two issues within my github repository I would like to resolve on my own moving forward.
+- Include some type of message to inform the user they have to select at least something for the password to generate. Even though I feel this is self explanatory which is why i did not include it here.
+- Add a reset button. Currently whenever the generate button is pressed to generate a brand new password you have to refresh the page. To fix this it would be a good idea to include a reset button next to the generate button to reset the password text to populate a brand new random password.
+
+### Web link:
+- My [website](https://gerushays.github.io/friendly-parakeet/)
+
+## Screenshot
+![Password-gen-Screenshot](./Develop/images/Password-gen-screenshot.png)
+
+## Contribution
+Made with ❤️ by Gerus Hays
--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -1,29 +1,30 @@
 // questions variable to set and cycle through the 5 questions 
-// title for the question, options for the answer choices and then answer is ofcourse... the answer
+// head for the question, options for the answer choices and then answer is ofcourse... the answer
+// all questions and answers from google
 var questions = [
     {
-        title: "Placeholder",
-        options: ["Placeholder", "placeholder", "this one", "this one too"],
-        answer: "Placeholder",
+        head: "1.   Javascript is an ________ language.",
+        options: ["Object-Oriented", "Object-Based", "Procedural", "All of the Above"],
+        answer: "Object-Oriented",
     },
     {
-        title: "Placeholder",
-        options: ["Placeholder", "placeholder", "this one", "this one too"],
-        answer: "Placeholder",
+        head: "2.   Upon encountering empty statements, what does the Javascript Interpreter do?",
+        options: ["Throws an error", "Ignores the statement", "Gives a warning", "None of the Above"],
+        answer: "Ignores the statement",
     },
     {
-        title: "Placeholder",
-        options: ["Placeholder", "placeholder", "this one", "this one too"],
-        answer: "Placeholder",
+        head: "3.   Which of the following methods can be used to display data in some form using Javascript?",
+        options: ["document.write()", "console.log()", "window.alert()", "All of the Above"],
+        answer: "All of the Above",
     },
     {
-        title: "Placeholder",
-        options: ["Placeholder", "placeholder", "this one", "this one too"],
-        answer: "Placeholder",
+        head: "4.   What does the Javascript “debugger” statement do?",
+        options: ["debug all errors upon starting the code", "acts as a breakpoint in the program", "it will debug the error in the current state if any", "All of the Above"],
+        answer: "acts as a breakpoint in the program",
     },
     {
-        title: "Placeholder",
-        options: ["Placeholder", "placeholder", "this one", "this one too"],
-        answer: "Placeholder",
+        head: "5.   What does … operator do in JS?",
+        options: ["it is used to describe a datatype of undefined size", "no such operator exists", "it is only used in command screens", "it is used to spread iterables to individual elements"],
+        answer: "it is used to spread iterables to individual elements",
     },
 ];
